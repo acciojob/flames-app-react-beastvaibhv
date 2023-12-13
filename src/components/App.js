2f987334977e8f1700e799b58a04450c2df34662
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-12-13 09:02:40 +0000

    flames-vaibhav

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,104 @@
-import React, {Component, useState} from "react";
+import React, { useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+const App= ()=> {
+    const[firstName, setFirstName] = useState("");
+    const[secondName, setSecondName] = useState("");
+    const[ans, setAns] = useState("");
+
+    const handleClear = ()=>{
+        setFirstName("");
+        setSecondName("");
+        setAns("");
+    }
+    const handleRelationship =()=>{
+        if(firstName.length === 0 || secondName.length === 0){
+             setAns("Please Enter valid input");
+        }
+        const f1 = firstName.toLowerCase();
+        const f2 = secondName.toLowerCase();
+
+        const count = countUncommonAlphabets(f1, f2);
+        
+        switch (count % 6) {
+            case 0:
+              setAns("Siblings")
+              break;
+            case 1:
+              setAns("Friends" )
+              break;
+              
+            case 2:
+              setAns("Love")
+              break;
+            case 3:
+              setAns("Affection")
+              break;
+                
+            case 4:
+              setAns("Marriage")
+              break;
+            
+            
+            default:
+              setAns("Enemy")
+              
+          }
+          
+          setFirstName("");
+          setSecondName('');
+
+    }
+    function countUncommonAlphabets(word1, word2) {
+      const count = new Map();
+      let totalLength = word1.length + word2.length;
+      for(let char in word1){
+        count.set(char, (count.get(char) || 0) + 1);
+      }
+      for(let char in word2){
+        if(count.has(char) && count.get(char)> 0){
+             count.set(char, (count.get(char) -1 ));
+             totalLength -= 2;
+        }
+      }
+      
+      
+      
+    
+      return totalLength ;
+    }
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+                <div>
+                    <input 
+                type="text" 
+                data-testid="input1" 
+                onChange={(e)=>{setFirstName(e.target.value)}}
+                value ={firstName}/>
+
+               <input 
+               data-testid="input2" 
+               type="text" 
+               onChange={(e)=>{setSecondName(e.target.value)}}
+               value ={secondName}/>
+
+               <button 
+               data-testid="calculate_relationship" 
+               onClick={handleRelationship}>
+                Calculate Relationship Future
+                </button>
+               <button 
+               data-testid="clear" 
+               onClick={handleClear}>
+                Clear
+                </button>
+                </div>
+               <div data-testid="answer"><h3>{ans}</h3></div>
+               
             </div>
         )
-    }
+    
 }
 
 
